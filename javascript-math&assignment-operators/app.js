269305b76540c2db7eaea97e7d856c68a69947bd
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-03-16 23:12:48 +0100

    Add files via upload

---
+++ javascript-math&assignment-operators/app.js
@@ -0,0 +1,33 @@
+var a =  1+2+3+4+5;
+document.write(a,"\n");
+
+
+// + is addition 
+// - is subtraction
+// / is division
+// * is multiplication
+// % is remainder
+// and more ...
+
+
+// Increment 
+var chop = 7;
+chop = chop + 1;
+// or you can do 
+chop++;
+document.write(chop,"\n");
+
+// Decrement
+chop--
+document.write(chop,"\n");
+
+
+
+// ASSIGNMENT OPERATORS
+var oldLevel = 5;
+oldLevel += 13;
+// we can also use other operators -,/,%,....
+oldLevel -= 10;
+oldLevel *= 2;
+document.write(oldLevel,"\n");
+
