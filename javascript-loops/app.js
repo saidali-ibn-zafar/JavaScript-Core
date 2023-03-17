2e94a8d462aa12cebda387f5a3ffd2932960f218
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-03-17 01:53:05 +0100

    Add files via upload

---
+++ javascript-loops/app.js
@@ -0,0 +1,20 @@
+// for loop
+for (i = 0; i < 10; i++){
+    document.write("I am for loop <br />");
+}
+
+// while loop
+let  x = 1;
+
+while (x<10){
+    document.write(x + "I am while loop <br />");
+    x++;
+}
+
+// do while loop
+let y = 5;
+
+do{
+    document.write(y + " hello <br />");
+    y++;
+}while(y<=20);
